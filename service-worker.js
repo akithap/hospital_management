0bcaa6d2aa887712b015f13ca223dddb9b5ff27b
akithap <akithaperera6@gmail.com>
2024-09-12 23:11:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,54 @@
-const CACHE_NAME = "my-site-cache-v1"; // Name of the cache
-const urlsToCache = [
-  "/hospital_management/", // Cache the root page
-  "/index.html",
-  "/styles.css",
-  "/script.js",
-  "/images/logo.png"
+// sw.js
+
+// Cache version
+const CACHE_NAME = 'my-cache-v1';
+
+// Files to cache
+const URLs_TO_CACHE = [
+  '/Hospital_management/',
+  '/Hospital_management/index.html',
+  '/Hospital_management/index.css',
+  '/Hospital-website/stript.js',
 ];
 
-// Install the service worker
-self.addEventListener("install", (event) => {
+// Install event
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache); // Add the files to the cache
+        console.log('Opened cache');
+        return cache.addAll(URLs_TO_CACHE);
       })
   );
 });
 
-// Fetch files from the cache or network
-self.addEventListener("fetch", (event) => {
+// Fetch event
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
+        // Cache hit - return response
         if (response) {
-          return response; // If the request is in the cache, return it
+          return response;
         }
-        return fetch(event.request); // Otherwise, fetch from the network
+        return fetch(event.request);
       })
   );
 });
 
-// Update the service worker
-self.addEventListener("activate", (event) => {
+// Activate event
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName); // Delete outdated caches
+            return caches.delete(cacheName);
           }
         })
       );
     })
   );
 });
+
